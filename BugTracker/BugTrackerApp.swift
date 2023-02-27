edf64062c4c7435c673189ae0b56b3eebb7f06a6
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-02-27 16:47:39 +0000

    Initial Commit

---
+++ BugTracker/BugTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugTrackerApp.swift
+//  BugTracker
+//
+//  Created by Jill Allan on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BugTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
