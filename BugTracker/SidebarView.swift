1e0e87df69195110bca42b9bdf0a76ea759aae31
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-07-05 16:17:38 +0100

    Update to show, delete and sync issues

--- BugTracker/SidebarView.swift
+++ BugTracker/SidebarView.swift
@@ -15,7 +15,7 @@ struct SidebarView: View {
     
     var tagFilters: [Filter] {
         tags.map { tag in
-            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No name", icon: "tag", tag: tag)
+            Filter(id: tag.tagID, name: tag.tagName, icon: "tag", tag: tag)
         }
     }
     
@@ -32,8 +32,10 @@ struct SidebarView: View {
                 ForEach(tagFilters) { filter in
                     NavigationLink(value: filter) {
                         Label(filter.name, systemImage: filter.icon)
+                            .badge(filter.tag?.tagActiveIssues.count ?? 0)
                     }
                 }
+                .onDelete(perform: delete)
             }
         }
         .toolbar {
@@ -46,11 +48,19 @@ struct SidebarView: View {
 
         }
     }
+    func delete(_ offsets: IndexSet) {
+        for offset in offsets {
+            let item = tags[offset]
+            dataController.delete(item)
+        }
+    }
 }
 
 struct SidebarView_Previews: PreviewProvider {
     static var previews: some View {
-        SidebarView()
-            .environmentObject(DataController.preview)
+        NavigationStack {
+            SidebarView()
+                .environmentObject(DataController.preview)
+        }
     }
 }
